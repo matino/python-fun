f915274bba7e33898fa31c6dfc07c93562105e66
matino <mczubak@gmail.com>
2015-02-17 09:28:27 +0100

    Update dining_philosophers.py

--- dining_philosophers.py
+++ dining_philosophers.py
@@ -6,26 +6,26 @@ NUM = 3
 
 
 class Fork(object):
-    def __init__(self, id):
+    def __init__(self, fork_id):
         self.lock = Lock()
-        self.id = id
+        self._id = fork_id
 
-    def grab(self, id):
+    def grab(self, person_id):
         self.lock.acquire()
-        print "Fork# %d was grabbed by philosopher# %d" % (self.id, id)
+        print "Fork# %d was grabbed by philosopher# %d" % (self._id, person_id)
 
-    def lay_down(self, id):
-        print "Fork# %d was layed down by philosopher# %d" % (self.id, id)
+    def lay_down(self, person_id):
+        print "Fork# %d was layed down by philosopher# %d" % (self._id, person_id)
         self.lock.release()
 
     def __str__(self):
-        return "Fork# %d" % self.id
+        return "Fork# %d" % self._id
 
 
 class Philosopher(Thread):
-    def __init__(self, id, left_fork, right_fork, sem, *args, **kwargs):
+    def __init__(self, id_, left_fork, right_fork, sem, *args, **kwargs):
         super(Philosopher, self).__init__(*args, **kwargs)
-        self.id = id
+        self._id = id_
         self.left_fork = left_fork
         self.right_fork = right_fork
         self.semaphore = sem
@@ -36,24 +36,24 @@ class Philosopher(Thread):
             self._eat()
 
     def _think(self):
-        print "Philosopher# %d is thinking" % self.id
+        print "Philosopher# %d is thinking" % self._id
         sleep(randint(3, 5))
 
     def _eat(self):
-        print "Philosopher# %d is hungry" % self.id
+        print "Philosopher# %d is hungry" % self._id
 
         with self.semaphore:
-            self.left_fork.grab(self.id)
+            self.left_fork.grab(self._id)
             sleep(3)
-            self.right_fork.grab(self.id)
+            self.right_fork.grab(self._id)
 
-            print "Philosopher# %d is eating" % self.id
+            print "Philosopher# %d is eating" % self._id
 
             sleep(randint(1, 3))
-            self.left_fork.lay_down(self.id)
-            self.right_fork.lay_down(self.id)
+            self.left_fork.lay_down(self._id)
+            self.right_fork.lay_down(self._id)
 
-            print "Philosopher# %d finished eating" % self.id
+            print "Philosopher# %d finished eating" % self._id
 
 
 if __name__ == "__main__":
